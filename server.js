adcd8d725afdc6c963e0250db0b58a0be858f17b
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-12-07 23:27:08 -0600

    Update server.js
    adding lists and items.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://lea_admin_willie:Dndmongodb_forlea44!@lea0-hs7rh.mongodb.net/to_do_list_db?retryWrites=true&w=majority'; 
+const mongoDB = 'mongodb+srv://MGengelbach:6zy6RKSSc3p@cluster0.lhqif.mongodb.net/<dbname>?retryWrites=true&w=majority'; 
 var List = require('./models/ToDo.js'); 
 var Item = require('./models/ToDoItem.js'); 
 
@@ -17,24 +17,36 @@ db.once('open', function(){
     console.log("We're connected");
     
     //create a new item instance using the Item model
-    let item1 = new Item({
+   /* let item1 = new Item({
         itemName     : "Do Dishes",
         itemPriority : "High" ,
         assignee     : "Willie",
         completed    : false  
     }); 
-
-    item1.save(function(err, item){
+    */
+//Here I've created my own new item
+    let item2 = new Item({
+        itemName     : "Finish mixing Christmas Song",
+        itemPriority : "High" ,
+        assignee     : "Mad",
+        completed    : false  
+    }); 
+//Here's the commmand to save and log the new item I created.
+    item2.save(function(err, item){
         if (err) return console.error(err);
         console.log(item); 
     }); 
 
+    let notComplete = Item.find({completed: false }, function(err, arr){});
+        console.log(notComplete); 
+ } ); 
+
     //create new list with our item. 
     var myList = new List({
-        name : "Willie's List",
+        name : "Mad's List",
         items : [
             {
-            item : item1._id 
+            item : item2._id 
             }
             ],
     }); 
@@ -47,4 +59,3 @@ db.once('open', function(){
     myList.save(); //add callback to error check if we wanted
 
     
-});
