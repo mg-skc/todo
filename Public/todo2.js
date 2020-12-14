04b808b53b7a6501dcb9f9b9dec9d0ec2badc5e1
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-12-14 00:30:46 -0600

    added buttons and more styling

--- Public/todo2.js
+++ Public/todo2.js
@@ -14,6 +14,25 @@ async function getToDoList(){
 
 }
 
+
+//app.post('/item', function(request, response){
+        //use the request data to create a new item 
+       //and add it to my database. 
+    //     console.log(request); 
+    //     let item1 = new Item({
+    //         itemName     : "Do Dishes",
+    //         itemPriority : "High" ,
+    //         assignee     : "Willie",
+    //         completed    : false  
+    //     }); 
+    
+    //     item1.save(function(err, item){
+    //         if (err) return console.error(err);
+    //         console.log(item); 
+    //     }); 
+    // });
+    
+
 async function itemCardDisplay(body) {
     // Function for display article data and generating a bootstrap card
     let card = "<div class='card bg-dark'>";
@@ -28,9 +47,9 @@ async function itemCardDisplay(body) {
     card += "</div>";
     card += "</div>";
 
-    // Append the new article card to the article section div
+    // Append the new item card to the item section section div
     $("#itemcards").append(card)
-}
+};
 
 function clickButton(){
    /* getToDoList().then(function(body){
@@ -46,5 +65,4 @@ function clickButton(){
     }).catch(function(err){
         console.log(err);
     });
-}
-
+};
